03de321ef7da4904f7ddc74b1eaed1b657e93a96
Agnijus <agnijus.botyrius01@gmail.com>
2023-12-19 16:25:36 +0000

    feat(api): init react-query and custom hook for fetching data

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,18 +1,11 @@
 import { useEffect } from "react";
 import customFetch from "../util";
+import { useFetchStocks } from "./reactQueryCustomHooks";
 
 const Overview = () => {
-  const fetchStocks = async () => {
-    const { data } = await customFetch.get(
-      "/time_series?symbol=tsla&interval=1month"
-    );
-    // const data = await res.json();
-    console.log(data);
-  };
+  const { isLoading, isError, data } = useFetchStocks();
 
-  useEffect(() => {
-    fetchStocks();
-  }, []);
+  console.log(data);
 
   return <div className="overview-container"></div>;
 };
---
+++ src/components/reactQueryCustomHooks.jsx
@@ -0,0 +1,15 @@
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import customFetch from "../util";
+
+export const useFetchStocks = () => {
+  const { data, isError, isLoading } = useQuery({
+    queryKey: ["stocks"],
+    queryFn: async () => {
+      const { data } = await customFetch.get(
+        "/time_series?symbol=tsla&interval=1month"
+      );
+      return data;
+    },
+  });
+  return { isLoading, isError, data };
+};
