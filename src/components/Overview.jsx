91c01629525bdab687b650289779ac93ed545a03
Agnijus <agnijus.botyrius01@gmail.com>
2023-12-19 16:11:05 +0000

    feat(api): added axios custom fetch with api key hidden

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,4 +1,19 @@
+import { useEffect } from "react";
+import customFetch from "../util";
+
 const Overview = () => {
+  const fetchStocks = async () => {
+    const { data } = await customFetch.get(
+      "/time_series?symbol=tsla&interval=1month"
+    );
+    // const data = await res.json();
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fetchStocks();
+  }, []);
+
   return <div className="overview-container"></div>;
 };
 export default Overview;
