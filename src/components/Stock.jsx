6aa1c2c25f57f110cb3df4e48dee7f41e6d0adfb
Agnijus <agnijus.botyrius01@gmail.com>
2023-12-20 21:26:37 +0000

    style(chart): adjusting tooltip and how data is displayed

--- src/components/Stock.jsx
+++ src/components/Stock.jsx
@@ -8,7 +8,7 @@ import { useState, useEffect } from "react";
 const data = {
   meta: {
     symbol: "TSLA",
-    interval: "1week",
+    interval: "1h",
     currency: "USD",
     exchange_timezone: "America/New_York",
     exchange: "NASDAQ",
@@ -17,244 +17,244 @@ const data = {
   },
   values: [
     {
-      datetime: "2023-12-18",
+      datetime: "2023-12-20 15:30:00",
+      open: "248.58000",
+      high: "249.45000",
+      low: "247.00000",
+      close: "247.23990",
+      volume: "12048845",
+    },
+    {
+      datetime: "2023-12-20 14:30:00",
+      open: "254.51640",
+      high: "254.59000",
+      low: "247.28999",
+      close: "248.55000",
+      volume: "28641275",
+    },
+    {
+      datetime: "2023-12-20 13:30:00",
+      open: "256.85849",
+      high: "257.44000",
+      low: "254.50000",
+      close: "254.50121",
+      volume: "10497567",
+    },
+    {
+      datetime: "2023-12-20 12:30:00",
+      open: "255.77000",
+      high: "256.92999",
+      low: "255.20000",
+      close: "256.85999",
+      volume: "10257948",
+    },
+    {
+      datetime: "2023-12-20 11:30:00",
+      open: "256.90390",
+      high: "257.67001",
+      low: "255.60001",
+      close: "255.78690",
+      volume: "12434477",
+    },
+    {
+      datetime: "2023-12-20 10:30:00",
+      open: "258.48001",
+      high: "258.92001",
+      low: "256.76010",
+      close: "256.90009",
+      volume: "16211001",
+    },
+    {
+      datetime: "2023-12-20 09:30:00",
+      open: "256.45001",
+      high: "259.84000",
+      low: "254.87010",
+      close: "258.48041",
+      volume: "30511696",
+    },
+    {
+      datetime: "2023-12-19 15:30:00",
+      open: "256.06009",
+      high: "257.25000",
+      low: "256.01001",
+      close: "257.19501",
+      volume: "6208436",
+    },
+    {
+      datetime: "2023-12-19 14:30:00",
+      open: "256.19711",
+      high: "256.89999",
+      low: "255.57150",
+      close: "256.10001",
+      volume: "8957140",
+    },
+    {
+      datetime: "2023-12-19 13:30:00",
+      open: "256.23001",
+      high: "256.95001",
+      low: "255.20000",
+      close: "256.17999",
+      volume: "11654701",
+    },
+    {
+      datetime: "2023-12-19 12:30:00",
+      open: "256.06500",
+      high: "256.45001",
+      low: "254.66000",
+      close: "256.22501",
+      volume: "11148023",
+    },
+    {
+      datetime: "2023-12-19 11:30:00",
+      open: "257.94000",
+      high: "258.14990",
+      low: "255.24001",
+      close: "256.04999",
+      volume: "14831676",
+    },
+    {
+      datetime: "2023-12-19 10:30:00",
+      open: "256.95999",
+      high: "258.33990",
+      low: "255.55000",
+      close: "257.92990",
+      volume: "20715436",
+    },
+    {
+      datetime: "2023-12-19 09:30:00",
+      open: "253.48000",
+      high: "256.97000",
+      low: "253.00999",
+      close: "256.96991",
+      volume: "29493260",
+    },
+    {
+      datetime: "2023-12-18 15:30:00",
+      open: "252.75999",
+      high: "252.89000",
+      low: "251.36000",
+      close: "252.02000",
+      volume: "7893149",
+    },
+    {
+      datetime: "2023-12-18 14:30:00",
+      open: "252.73000",
+      high: "253.62000",
+      low: "252.14000",
+      close: "252.78461",
+      volume: "12845087",
+    },
+    {
+      datetime: "2023-12-18 13:30:00",
+      open: "254.14500",
+      high: "254.97000",
+      low: "252.64011",
+      close: "252.71359",
+      volume: "12026431",
+    },
+    {
+      datetime: "2023-12-18 12:30:00",
+      open: "254.50000",
+      high: "255.37939",
+      low: "254.02000",
+      close: "254.14430",
+      volume: "12031644",
+    },
+    {
+      datetime: "2023-12-18 11:30:00",
+      open: "255.78999",
+      high: "256.37411",
+      low: "253.25060",
+      close: "254.50999",
+      volume: "17112510",
+    },
+    {
+      datetime: "2023-12-18 10:30:00",
+      open: "258.39001",
+      high: "258.48999",
+      low: "255.48010",
+      close: "255.78000",
+      volume: "18012954",
+    },
+    {
+      datetime: "2023-12-18 09:30:00",
       open: "253.78000",
       high: "258.73999",
-      low: "251.36000",
-      close: "256.47009",
-      volume: "229863955",
+      low: "252.56000",
+      close: "258.39001",
+      volume: "32467551",
     },
     {
-      datetime: "2023-12-11",
-      open: "242.74001",
+      datetime: "2023-12-15 15:30:00",
+      open: "251.88000",
       high: "254.13000",
-      low: "228.20000",
-      close: "253.50000",
-      volume: "636078500",
+      low: "251.86000",
+      close: "253.34000",
+      volume: "10078455",
     },
     {
-      datetime: "2023-12-04",
-      open: "235.75000",
-      high: "246.66000",
-      low: "233.28999",
-      close: "243.84000",
-      volume: "578629500",
+      datetime: "2023-12-15 14:30:00",
+      open: "250.70480",
+      high: "252.30000",
+      low: "250.67999",
+      close: "251.89079",
+      volume: "8573371",
     },
     {
-      datetime: "2023-11-27",
-      open: "236.89000",
-      high: "252.75000",
-      low: "231.89999",
-      close: "238.83000",
-      volume: "649509700",
+      datetime: "2023-12-15 13:30:00",
+      open: "252.78999",
+      high: "252.97990",
+      low: "250.60001",
+      close: "250.71719",
+      volume: "10947852",
     },
     {
-      datetime: "2023-11-20",
-      open: "234.03999",
-      high: "244.00999",
-      low: "231.02000",
-      close: "235.45000",
-      volume: "421683900",
+      datetime: "2023-12-15 12:30:00",
+      open: "253.19000",
+      high: "253.67999",
+      low: "251.78000",
+      close: "252.80991",
+      volume: "13224094",
     },
     {
-      datetime: "2023-11-13",
-      open: "215.60001",
-      high: "246.70000",
-      low: "211.61000",
-      close: "234.30000",
-      volume: "719922800",
+      datetime: "2023-12-15 11:30:00",
+      open: "251.87000",
+      high: "253.37000",
+      low: "250.82001",
+      close: "253.19321",
+      volume: "14197106",
     },
     {
-      datetime: "2023-11-06",
-      open: "223.98000",
-      high: "226.32001",
-      low: "205.69000",
-      close: "214.64999",
-      volume: "613925200",
+      datetime: "2023-12-15 10:30:00",
+      open: "252.80000",
+      high: "253.30000",
+      low: "250.14000",
+      close: "251.87000",
+      volume: "17981872",
     },
     {
-      datetime: "2023-10-30",
-      open: "209.28000",
-      high: "226.37010",
-      low: "191.25000",
-      close: "219.96001",
-      volume: "621446800",
+      datetime: "2023-12-15 09:30:00",
+      open: "250.37500",
+      high: "253.07001",
+      low: "248.30000",
+      close: "252.79050",
+      volume: "39296941",
     },
     {
-      datetime: "2023-10-23",
-      open: "210.00000",
-      high: "222.05000",
-      low: "202.50999",
-      close: "207.30000",
-      volume: "585973400",
+      datetime: "2023-12-14 15:30:00",
+      open: "251.10001",
+      high: "251.55000",
+      low: "249.91000",
+      close: "251.14011",
+      volume: "9127180",
     },
     {
-      datetime: "2023-10-16",
-      open: "250.05000",
-      high: "257.17999",
-      low: "210.42000",
-      close: "211.99001",
-      volume: "616134600",
-    },
-    {
-      datetime: "2023-10-09",
-      open: "255.31000",
-      high: "268.94000",
-      low: "250.22000",
-      close: "251.12000",
-      volume: "541322100",
-    },
-    {
-      datetime: "2023-10-02",
-      open: "244.81000",
-      high: "263.60001",
-      low: "242.62000",
-      close: "260.53000",
-      volume: "592623500",
-    },
-    {
-      datetime: "2023-09-25",
-      open: "243.38000",
-      high: "254.77000",
-      low: "234.58000",
-      close: "250.22000",
-      volume: "588632500",
-    },
-    {
-      datetime: "2023-09-18",
-      open: "271.16000",
-      high: "273.92999",
-      low: "244.48000",
-      close: "244.88000",
-      volume: "574817000",
-    },
-    {
-      datetime: "2023-09-11",
-      open: "264.26999",
-      high: "278.98001",
-      low: "260.60999",
-      close: "274.39001",
-      volume: "663474100",
-    },
-    {
-      datetime: "2023-09-04",
-      open: "245.00000",
-      high: "258.00000",
-      low: "243.27000",
-      close: "248.50000",
-      volume: "480110000",
-    },
-    {
-      datetime: "2023-08-28",
-      open: "242.58000",
-      high: "261.17999",
-      low: "235.35001",
-      close: "245.00999",
-      volume: "604843900",
-    },
-    {
-      datetime: "2023-08-21",
-      open: "221.55000",
-      high: "240.82001",
-      low: "220.58000",
-      close: "238.59000",
-      volume: "573767800",
-    },
-    {
-      datetime: "2023-08-14",
-      open: "235.70000",
-      high: "240.66000",
-      low: "212.36000",
-      close: "215.49001",
-      volume: "555809500",
-    },
-    {
-      datetime: "2023-08-07",
-      open: "251.45000",
-      high: "253.64999",
-      low: "238.02000",
-      close: "242.64999",
-      volume: "517701600",
-    },
-    {
-      datetime: "2023-07-31",
-      open: "267.48001",
-      high: "269.07999",
-      low: "250.49001",
-      close: "253.86000",
-      volume: "466312800",
-    },
-    {
-      datetime: "2023-07-24",
-      open: "255.85001",
-      high: "272.89999",
-      low: "254.12000",
-      close: "266.44000",
-      volume: "560265300",
-    },
-    {
-      datetime: "2023-07-17",
-      open: "286.63000",
-      high: "299.29001",
-      low: "255.80000",
-      close: "260.01999",
-      volume: "722568100",
-    },
-    {
-      datetime: "2023-07-10",
-      open: "276.47000",
-      high: "285.29999",
-      low: "265.10001",
-      close: "281.38000",
-      volume: "539522500",
-    },
-    {
-      datetime: "2023-07-03",
-      open: "276.48999",
-      high: "284.25000",
-      low: "272.88000",
-      close: "274.42999",
-      volume: "485150900",
-    },
-    {
-      datetime: "2023-06-26",
-      open: "250.07001",
-      high: "264.45001",
-      low: "240.70000",
-      close: "261.76999",
-      volume: "748280600",
-    },
-    {
-      datetime: "2023-06-19",
-      open: "261.50000",
-      high: "276.98999",
-      low: "248.25000",
-      close: "256.60001",
-      volume: "720868300",
-    },
-    {
-      datetime: "2023-06-12",
-      open: "247.94000",
-      high: "263.60001",
-      low: "244.59000",
-      close: "260.54001",
-      volume: "811032600",
-    },
-    {
-      datetime: "2023-06-05",
-      open: "217.80000",
-      high: "252.42000",
-      low: "212.53000",
-      close: "244.39999",
-      volume: "848137500",
-    },
-    {
-      datetime: "2023-05-29",
-      open: "200.10001",
-      high: "217.25000",
-      low: "195.12000",
-      close: "213.97000",
-      volume: "591689300",
+      datetime: "2023-12-14 14:30:00",
+      open: "250.05400",
+      high: "252.14999",
+      low: "249.77000",
+      close: "251.10400",
+      volume: "15207547",
     },
   ],
   status: "ok",
@@ -269,14 +269,11 @@ const Stock = () => {
   }, [data]);
 
   const processData = (data) => {
-    const stockData = data.values
-      .slice()
-      .reverse()
-      .map((item) => ({
-        ...item,
-        datetime: new Date(item.datetime),
-        close: parseFloat(item.close),
-      }));
+    const stockData = data.values.map((item) => ({
+      ...item,
+      datetime: new Date(item.datetime),
+      close: parseFloat(item.close),
+    }));
 
     let minVal = Number.MAX_VALUE;
     let maxVal = Number.MIN_VALUE;
@@ -298,23 +295,39 @@ const Stock = () => {
   };
 
   return (
-    <div className="container sample">
-      <div className="container" style={{ height: "300px" }}>
-        <IgrFinancialChart
-          width="100%"
-          height="100%"
-          chartType="Line"
-          thickness={2}
-          chartTitle={data.meta.symbol}
-          yAxisMode={0}
-          yAxisTitle="Price"
-          xAxisTitle="Time"
-          dataSource={chartData}
-          isToolbarVisible={false}
-          markerTypes={[MarkerType.None]}
-          zoomSliderType={0}
-        />
+    <div
+      className="stock-chart"
+      style={{ height: "200px", marginLeft: "-40px" }}
+    >
+      <div>
+        <div className="stock-title">Tesla</div>
+        <div className="stock-price">255.95</div>
       </div>
+      <IgrFinancialChart
+        width="100%"
+        height="100%"
+        chartType="Line"
+        thickness={2}
+        titleAlignment="Left"
+        yAxisMode="Numeric"
+        dataSource={chartData}
+        isToolbarVisible={false}
+        markerTypes={[MarkerType.None]}
+        zoomSliderType="None"
+        xAxisInverted={true}
+        yAxisLabelLocation="OutsideLeft"
+        crosshairsSnapToData={false}
+        crosshairsDisplayMode="Both"
+        crosshairsAnnotationEnabled={true}
+        finalValueAnnotationsVisible={false}
+        calloutsVisible={true}
+        isHorizontalZoomEnabled={false}
+        dataToolTipValueFormatMinFractions={2}
+        dataToolTipHeaderText=""
+        dataToolTipUnitsText={data.meta.currency}
+        toolTipType="None"
+        isSeriesHighlightingEnabled={false}
+      />
     </div>
   );
 };
