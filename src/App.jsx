277789d5a527817a0709ebc0f3a09e9b5b393253
Agnijus <agnijus.botyrius01@gmail.com>
2023-12-27 20:11:58 +0000

    test(api): changed import.meta to process.env to test

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
 
   useEffect(() => {
     dispatch(fetchStockData());
-    // dispatch(searchStocks());
+    dispatch(searchStocks());
   }, []);
 
   useEffect(() => {
--- src/util.jsx
+++ src/util.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const customFetch = axios.create({
   baseURL: "https://api.twelvedata.com",
   headers: {
-    Authorization: `apikey ${import.meta.env.VITE_APP_API_KEY}`,
+    Authorization: `apikey ${process.env.VITE_APP_API_KEY}`,
   },
 });
 
