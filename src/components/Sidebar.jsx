28dcbeccdb6bbcbf75d364d6230e9a53abb204ff
Agnijus <agnijus.botyrius01@gmail.com>
2023-12-28 03:12:22 +0000

    feat(sidebar): added icons for navigation and hover/active styling

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,6 +2,10 @@ import { IoClose } from "react-icons/io5";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleSidebar } from "./features/menu/menuSlice";
 import { NavLink } from "react-router-dom";
+import { FaHome } from "react-icons/fa";
+import { TiStarFullOutline } from "react-icons/ti";
+import { IoNewspaper } from "react-icons/io5";
+import { BiSolidGraduation } from "react-icons/bi";
 
 const Sidebar = () => {
   const dispatch = useDispatch();
@@ -10,24 +14,40 @@ const Sidebar = () => {
     <aside className={`sidebar-container ${isSideBarOpened ? "active" : ""}`}>
       <div className="sidebar-center">
         <header>
-          <h3 className="title">Stocky</h3>
+          <h3 className="title">Stockist</h3>
 
           <IoClose
             className="sidebar-close"
             onClick={() => dispatch(toggleSidebar())}
           />
         </header>
-        <NavLink className="link" to="/">
-          Home
+        <NavLink
+          className={({ isActive }) => `link ${isActive && "is-active"}`}
+          to="/"
+        >
+          <FaHome className="link-icon" />
+          <span className="link-title">Home</span>
         </NavLink>
-        <NavLink className="link" to="/wishlist">
-          WishList
+        <NavLink
+          className={({ isActive }) => `link ${isActive && "is-active"}`}
+          to="/wishlist"
+        >
+          <TiStarFullOutline className="link-icon" />
+          <span className="link-title">WishList</span>
         </NavLink>
-        <NavLink className="link" to="/news">
-          News
+        <NavLink
+          className={({ isActive }) => `link ${isActive && "is-active"}`}
+          to="/news"
+        >
+          <IoNewspaper className="link-icon" />
+          <span className="link-title"> News</span>
         </NavLink>
-        <NavLink className="link" to="/educational">
-          Educational
+        <NavLink
+          className={({ isActive }) => `link ${isActive && "is-active"}`}
+          to="/educational"
+        >
+          <BiSolidGraduation className="link-icon" />
+          <span className="link-title">Educational</span>
         </NavLink>
       </div>
     </aside>
