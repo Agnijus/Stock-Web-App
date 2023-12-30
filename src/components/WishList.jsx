ffbbff9248d905a6916f54c085736f15bc4053de
Agnijus <agnijus.botyrius01@gmail.com>
2023-12-30 21:37:19 +0000

    style(WishList): grid adjustment and icon added

--- src/components/WishList.jsx
+++ src/components/WishList.jsx
@@ -6,6 +6,7 @@ import {
 } from "./features/stock/stockSlice";
 import { FaArrowDown } from "react-icons/fa6";
 import { FaArrowUp } from "react-icons/fa6";
+import { TiStarFullOutline } from "react-icons/ti";
 
 const WishList = () => {
   const dispatch = useDispatch();
@@ -36,13 +37,15 @@ const WishList = () => {
         } = value;
         return (
           <div key={key} className="wishlist-item">
-            <div>
-              <span className="wishlist-name">{name}</span>
-              <span className="wishlist-symbol-exchange">
-                {`${symbol}: ${exchange}`}
-              </span>
+            <div className="wishlist-top-line">
+              <div>
+                <span className="wishlist-name">{name}</span>
+                <span className="wishlist-symbol-exchange">
+                  {`${symbol}: ${exchange}`}
+                </span>
+              </div>
+              <TiStarFullOutline className="wishlist-icon" />
             </div>
-
             <div>
               <span className="wishlist-close">
                 {parseFloat(close).toFixed(2)}
